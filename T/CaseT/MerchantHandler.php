23c97dd2d4194ceef694807aa3c42d10ea28fb66
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-26 05:41:30 +0600

    no message

--- T/CaseT/MerchantHandler.php
+++ T/CaseT/MerchantHandler.php
@@ -3,4 +3,9 @@ namespace Dfe\TBCBank\T\CaseT;
 /**
  * 2018-09-26
  */
-final class MerchantHandler extends \Dfe\TBCBank\T\CaseT {}
+final class MerchantHandler extends \Dfe\TBCBank\T\CaseT {
+	/** @test 2018-09-26 */
+	function t00() {}
+
+	
+}
