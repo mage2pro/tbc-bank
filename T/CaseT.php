fb02f8533c03b1bf32b52e71e2a3f81eee3cb6ac
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-26 05:17:26 +0600

    no message

--- T/CaseT.php
+++ T/CaseT.php
@@ -2,6 +2,7 @@
 namespace Dfe\TBCBank\T;
 /**
  * 2018-09-26
+ * @see \Dfe\TBCBank\T\CaseT\MerchantHandler
  * @method \Dfe\AlphaCommerceHub\Method m()
  */
 abstract class CaseT extends \Df\Payment\TestCase {}
---
+++ T/CaseT/MerchantHandler.php
@@ -0,0 +1,6 @@
+<?php
+namespace Dfe\TBCBank\T\CaseT;
+/**
+ * 2018-09-26
+ */
+final class MerchantHandler extends \Dfe\TBCBank\T\CaseT {}
