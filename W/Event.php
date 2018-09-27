40df9ce4d5f3d65040be9009c0e330180945d100
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-27 23:13:17 +0600

    no message

--- W/Event.php
+++ W/Event.php
@@ -2,4 +2,13 @@
 namespace Dfe\TBCBank\W;
 use Magento\Sales\Model\Order\Payment\Transaction as T;
 // 2018-09-27
-final class Event extends \Df\PaypalClone\W\Event {}
+final class Event extends \Df\PaypalClone\W\Event {
+	/**
+	 * 2018-09-27
+	 * This method is never used: @see validate()
+	 * @override
+	 * @see \Df\PaypalClone\W\Event::k_signature()
+	 * @return string
+	 */
+	protected function k_signature() {return null;}
+}
