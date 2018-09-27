3d5f42c10cbe1e471243bdbb2baacf2099c641f7
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-27 21:19:52 +0600

    no message

--- Api.php
+++ Api.php
@@ -4,22 +4,18 @@ namespace Dfe\TBCBank;
 final class Api {
 	/**       
 	 * 2018-09-27
+	 * @used-by \Dfe\TBCBank\Init\Action::transIdE()
 	 * @used-by \Dfe\TBCBank\T\CaseT\CheckResult::t01()
 	 * @used-by \Dfe\TBCBank\T\CaseT\Init::transId()
-	 * @param string $command
 	 * @param array(string => string) $p
 	 * @return string
 	 */
-	static function p($command, array $p) {
+	static function p(array $p) {
 		$c = curl_init(); /** @var resource $c */
 		$s = dfps(__CLASS__); /** @var Settings $s */
 		// 2018-09-26
 		// https://stackoverflow.com/questions/5224790/curl-post-format-for-curlopt-postfields#comment35249942_5224940
-		curl_setopt($c, CURLOPT_POSTFIELDS, http_build_query($p + [
-			// 2018-09-26 «client’s IP address, mandatory (15 characters)»
-			'client_ip_addr' => df_visitor_ip()
-			,'command' => $command
-		]));
+		curl_setopt($c, CURLOPT_POSTFIELDS, http_build_query($p));
 		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($c, CURLOPT_SSL_VERIFYHOST, '0');
 		curl_setopt($c, CURLOPT_SSL_VERIFYPEER, '0');
--- Init/Action.php
+++ Init/Action.php
@@ -1,6 +1,7 @@
 <?php
 namespace Dfe\TBCBank\Init;
 use Df\Payment\W\Event as Ev;
+use Dfe\TBCBank\Api;
 use Dfe\TBCBank\Charge;
 use Dfe\TBCBank\Method as M;
 /**
@@ -37,26 +38,12 @@ final class Action extends \Df\Payment\Init\Action {
 	 * @return string
 	 */
 	private function transIdE() {return dfc($this, function() {
-		$curl = curl_init();
-		$submit_url = "https://ecommerce.ufc.ge:18443/ecomm2/MerchantHandler";
 		/** @var M $m */ /** @var array(string => mixed) $req */
 		df_sentry_extra($m = $this->m(), 'Request Params', $req = Charge::p($m));
 		$m->iiaSetTRR($req);
 		// 2018-09-26
-		// https://stackoverflow.com/questions/5224790/curl-post-format-for-curlopt-postfields#comment35249942_5224940
-		curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($req));
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, '0');
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, '0');
-		curl_setopt($curl, CURLOPT_SSLCERT, $this->s()->certificate());
-		curl_setopt($curl, CURLOPT_SSLKEYPASSWD, $this->s()->password());
-		curl_setopt($curl, CURLOPT_SSLVERSION, 1);
-		curl_setopt($curl, CURLOPT_TIMEOUT, 120);
-		curl_setopt($curl, CURLOPT_URL, $submit_url);
-		curl_setopt($curl, CURLOPT_VERBOSE, '0');
-		// 2018-09-26
 		// The server responds with a string like «TRANSACTION_ID: HOjPnNRq9KHNDKVnomSQUtShijw=».
 		// A transaction ID always contains 28 characters.
-		return substr(curl_exec($curl), -28);
+		return substr(Api::p($req), -28);
 	});}
 }
