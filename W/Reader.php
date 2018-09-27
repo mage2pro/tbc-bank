e3ca2d3f6c59cebf9c0f329c887d389eaef877c8
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-27 22:57:05 +0600

    no message

--- W/Reader.php
+++ W/Reader.php
@@ -1,4 +1,18 @@
 <?php
 namespace Dfe\TBCBank\W;
 // 2018-09-27
-final class Reader extends \Df\Payment\W\Reader {}
+final class Reader extends \Df\Payment\W\Reader {
+	/**
+	 * 2018-09-27
+	 * @override
+	 * @see \Df\Payment\W\Reader::reqFilter()
+	 * @used-by \Df\Payment\W\Reader::__construct()
+	 * @param array(string => mixed) $r
+	 * @return array(string => mixed)
+	 */
+	protected function reqFilter(array $r) {
+		if (isset($r['trans_id'])) {
+		}
+		return $r;
+	}
+}
