812ff7d4f632a6106cfbb52a44fd7278904bcda8
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-28 17:36:30 +0600

    no message

--- ConfigProvider.php
+++ ConfigProvider.php
@@ -1,4 +1,15 @@
 <?php
 namespace Dfe\TBCBank;
-// 2017-04-18
-final class ConfigProvider extends \Df\Payment\ConfigProvider {}
+// 2018-09-28
+final class ConfigProvider extends \Df\Payment\ConfigProvider {
+	/**
+	 * 2018-09-28
+	 * @override
+	 * @see \Df\Payment\ConfigProvider::config()
+	 * @used-by \Df\Payment\ConfigProvider::getConfig()
+	 * @return array(string => mixed)
+	 */
+	protected function config() {return [
+		'pid' => urlencode('ZdmOxgSlAkkzvgc2DEeKBHLSZOo=')
+	] + parent::config();}
+}
