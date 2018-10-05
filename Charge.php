f82a473df76e8dbc28be6668fda18d1d5c488fbc
Dmitry Fedyuk <dfediuk@gmail.com>
2018-10-06 04:24:46 +0600

    no message

--- Charge.php
+++ Charge.php
@@ -1,5 +1,6 @@
 <?php
 namespace Dfe\TBCBank;
+use Df\Payment\Init\Action;
 // 2018-09-26
 /** @method Method m() */
 final class Charge extends \Df\Payment\Charge {
@@ -12,7 +13,8 @@ final class Charge extends \Df\Payment\Charge {
 		'amount' => $this->amountF()
 		// 2018-09-26 «client’s IP address, mandatory (15 characters)»
 		,'client_ip_addr' => df_visitor_ip()
-		,'command' => 'v' // 2018-09-26 «identifies a request for transaction registration»
+		 // 2018-09-26 «identifies a request for transaction registration»
+		,'command' => Action::sg($this->m())->preconfiguredToCapture() ? 'v' : 'a'
 		// 2018-09-26 «transaction currency code (ISO 4217), mandatory, (3 digits)»
 		,'currency' => df_currency_num($this->currencyC())
 		,'description' => 'UFCTEST' // 2018-09-26 «transaction details, optional (up to 125 characters)»
