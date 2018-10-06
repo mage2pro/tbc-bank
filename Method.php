1dda030331c53f5af30326987415bb3ad90d181e
Dmitry Fedyuk <dfediuk@gmail.com>
2018-10-06 08:22:02 +0600

    no message

--- Method.php
+++ Method.php
@@ -4,6 +4,46 @@ use Df\Payment\Token;
 // 2018-09-26
 final class Method extends \Df\Payment\Method {
 	/**
+	 * 2018-10-06
+	 * "Implement an ability to capture a preauthorized bank card payment from the Magento backend
+	 * (the `CapturePayment` transaction)": https://github.com/mage2pro/alphacommercehub/issues/60
+	 * @override
+	 * @see \Df\Payment\Method::canCapture()
+	 * 1) @used-by \Magento\Sales\Model\Order\Payment::canCapture():
+	 *		if (!$this->getMethodInstance()->canCapture()) {
+	 *			return false;
+	 *		}
+	 * https://github.com/magento/magento2/blob/2.0.0/app/code/Magento/Sales/Model/Order/Payment.php#L246-L269
+	 * https://github.com/magento/magento2/blob/2.2.1/app/code/Magento/Sales/Model/Order/Payment.php#L277-L301
+	 * 2) @used-by \Magento\Sales\Model\Order\Payment::_invoice():
+	 *		protected function _invoice() {
+	 *			$invoice = $this->getOrder()->prepareInvoice();
+	 *			$invoice->register();
+	 *			if ($this->getMethodInstance()->canCapture()) {
+	 *				$invoice->capture();
+	 *			}
+	 *			$this->getOrder()->addRelatedObject($invoice);
+	 *			return $invoice;
+	 *		}
+	 * https://github.com/magento/magento2/blob/2.0.0/app/code/Magento/Sales/Model/Order/Payment.php#L509-L526
+	 * https://github.com/magento/magento2/blob/2.2.1/app/code/Magento/Sales/Model/Order/Payment.php#L542-L560
+	 * 3) @used-by \Magento\Sales\Model\Order\Payment\Operations\AbstractOperation::invoice():
+	 *		protected function invoice(OrderPaymentInterface $payment) {
+	 *			$invoice = $payment->getOrder()->prepareInvoice();
+	 *			$invoice->register();
+	 *			if ($payment->getMethodInstance()->canCapture()) {
+	 *				$invoice->capture();
+	 *			}
+	 *			$payment->getOrder()->addRelatedObject($invoice);
+	 *			return $invoice;
+	 *		}
+	 * https://github.com/magento/magento2/blob/2.0.0/app/code/Magento/Sales/Model/Order/Payment/Operations/AbstractOperation.php#L56-L75
+	 * https://github.com/magento/magento2/blob/2.2.1/app/code/Magento/Sales/Model/Order/Payment/Operations/AbstractOperation.php#L59-L78
+	 * @return bool
+	 */
+	final function canCapture() {return true;}
+
+	/**
 	 * 2018-09-26
 	 * @override
 	 * @see \Df\Payment\Method::amountLimits()
