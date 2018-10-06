612ca477e892eec974035b394c68603bb44fd2e8
Dmitry Fedyuk <dfediuk@gmail.com>
2018-10-06 08:39:30 +0600

    no message

--- Method.php
+++ Method.php
@@ -1,6 +1,8 @@
 <?php
 namespace Dfe\TBCBank;
 use Df\Payment\Token;
+use Df\Payment\W\Event as Ev;
+use Magento\Sales\Model\Order\Payment\Transaction as T;
 // 2018-09-26
 final class Method extends \Df\Payment\Method {
 	/**
@@ -63,6 +65,47 @@ final class Method extends \Df\Payment\Method {
 	 * @return bool
 	 */
 	function canVoid() {return true;}
+
+	/**
+	 * 2018-10-06 It captures a preauthorized bank card payment.
+	 * @override
+	 * @see \Df\Payment\Method::charge()
+	 * @used-by \Df\Payment\Method::authorize()
+	 * @used-by \Df\Payment\Method::capture()
+	 * @param bool|null $capture [optional]
+	 */
+	function charge($capture = true) {
+		df_assert($capture);
+		df_sentry_extra($this, 'Amount', $a = dfp_due($this)); /** @var float $a */
+		df_sentry_extra($this, 'Need Capture?', df_bts($capture));
+		/**
+		 * 2017-11-11
+		 * Despite of its name, @uses \Magento\Sales\Model\Order\Payment::getAuthorizationTransaction()
+		 * simply returns the previous transaction, and it can be not only an `authorization` transaction,
+		 * but a transaction of another type (e.g. `payment`) too.
+		 * https://github.com/mage2pro/core/blob/3.4.2/StripeClone/Method.php#L124-L159
+		 */
+		$tPrev = $this->ii()->getAuthorizationTransaction(); /** @var T|false|null $tPrev */
+		/**
+		 * 2017-11-11
+		 * The @see \Magento\Sales\Api\Data\TransactionInterface::TYPE_AUTH constant
+		 * is absent in Magento < 2.1.0,
+		 * but is present as @uses \Magento\Sales\Model\Order\Payment\Transaction::TYPE_AUTH
+		 * https://github.com/magento/magento2/blob/2.0.0/app/code/Magento/Sales/Model/Order/Payment/Transaction.php#L37
+		 * https://github.com/magento/magento2/blob/2.0.17/app/code/Magento/Sales/Api/Data/TransactionInterface.php
+		 */
+		df_assert($tPrev && T::TYPE_AUTH === $tPrev->getTxnType());
+		df_sentry_extra($this, 'Parent Transaction ID', $txnId = $tPrev->getTxnId()); /** @var string $txnId */
+		df_sentry_extra($this, 'Charge ID', $tid = $this->tid()->i2e($txnId, true)); /** @var string $tid */
+		//$this->transInfo(fBankCard::s()->capture($a));
+		// 2016-12-16
+		// Система в этом сценарии по-умолчанию формирует идентификатор транзации как
+		// «<идентификатор родительской транзации>-capture».
+		// У нас же идентификатор родительской транзации имеет окончание «-authorize»,
+		// и оно нам реально нужно (смотрите комментарий к ветке else ниже),
+		// поэтому здесь мы окончание «-authorize» вручную подменяем на «-capture».
+		$this->ii()->setTransactionId($this->tid()->e2i($tid, Ev::T_CAPTURE));
+	}
 
 	/**
 	 * 2018-09-26
