c4f725a71b52f39e907372228d69835c93fdcc97
Dmitry Fedyuk <dfediuk@gmail.com>
2019-07-05 13:45:51 +0600

    1.4.2

--- W/Strategy/ConfirmPending.php
+++ W/Strategy/ConfirmPending.php
@@ -13,7 +13,7 @@ final class ConfirmPending extends \Df\Payment\W\Strategy\ConfirmPending {
 	 * @see \Df\Payment\W\Strategy\ConfirmPending::onSuccess()
 	 * @used-by \Df\Payment\W\Strategy\ConfirmPending::_handle()
 	 */
-	function onSuccess() {
+	protected function onSuccess() {
 		$e = $this->e(); /** @var E $e */
 		if ($id = $e->cardId()) { /** @var string $id */
 			df_ci_save($this, [$id => $e->r([E::CARD_NUMBER, E::CARD_EXP, E::CARD_ID])]);
