964a3bc1b50a19f67942ca0fe18a212e3b62af08
Dmitry Fedyuk <dfediuk@gmail.com>
2021-10-31 04:06:39 +0600

    1.4.7: https://github.com/mage2pro/core/issues/163

--- Session.php
+++ Session.php
@@ -1,7 +1,7 @@
 <?php
 namespace Dfe\TBCBank;
 # 2021-10-27 "Improve the custom session data handling interface": https://github.com/mage2pro/core/issues/163
-final class Session extends \Df\Core\Session {
+final class Session extends \Df\Customer\SessionBase {
 	/**
 	 * 2021-10-27
 	 * @used-by \Dfe\TBCBank\Init::p()
@@ -10,13 +10,4 @@ final class Session extends \Df\Core\Session {
 	 * @return $this|array(string => mixed)
 	 */
 	function data($v = DF_N) {return df_prop($this, $v, []);}
-
-	/**
-	 * 2021-10-26
-	 * @override
-	 * @see \Df\Core\Session::c()
-	 * @used-by \Df\Core\Session::__construct()
-	 * @return string
-	 */
-	protected function c() {return \Magento\Customer\Model\Session\Storage::class;}
 }
