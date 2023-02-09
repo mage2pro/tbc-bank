239898ca38d57f80efb1d36990e250bd531e556f
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-09 07:12:35 +0000

    https://github.com/mage2pro/core/issues/174

--- Init.php
+++ Init.php
@@ -14,7 +14,6 @@ final class Init {
 	 * $qp в поле @see \Magento\Framework\DataObject::_data содержит код способа оплаты,
 	 * а также ту дополнительную информацию, которую передала клиентская часть модуля оплаты.
 	 * Например: [additional_data => [], method => "dfe_klarna"].
-	 * @param IQA|QA|null $ba
 	 * 2017-04-05 Важно возвращать именно string: @see dfw_encode()
 	 * @return string
 	 */
