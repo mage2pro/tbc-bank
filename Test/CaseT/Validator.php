ceef52dfb4b9e8bdc0b8a8b711b17e2d17f5a403
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-09 08:11:48 +0000

    https://github.com/mage2pro/core/issues/168

--- Test/CaseT/Validator.php
+++ Test/CaseT/Validator.php
@@ -6,7 +6,7 @@ final class Validator extends \Dfe\TBCBank\Test\CaseT {
 	function t00():void {}
 
 	/** 2018-11-11 */
-	function t01():string {echo df_json_encode([
+	function t01():void {echo df_json_encode([
 		df_preg_prefix('error: ', "error: parameter 'command' not specified")
 		,df_preg_prefix('error: ', 'TRANSACTION_ID: 6ZysGdr05Lvo6p2ieDvg7/fzdeU=')
 		,df_preg_prefix('TRANSACTION_ID: ', "error: parameter 'command' not specified")
