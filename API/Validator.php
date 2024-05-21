a0231189cd96eef89c9d5811a147965b5705a484
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-22 00:50:55 +0100

    refactoring

--- API/Validator.php
+++ API/Validator.php
@@ -12,6 +12,7 @@ final class Validator extends \Df\API\Response\Validator {
 	 * @see \Df\API\Exception::long()
 	 * @used-by self::valid()
 	 * @used-by \Df\API\Client::_p()
+	 * @used-by \Df\API\Exception::message()
 	 */
 	function long():string {return df_nts($this->r('error') ?: ('FAILED' !== $this->r('RESULT') ? null : (
 		(string)__(($c = $this->r('RESULT_CODE')) ? "Dfe_TBCBank_R_$c" : 'An unknown error occured')
